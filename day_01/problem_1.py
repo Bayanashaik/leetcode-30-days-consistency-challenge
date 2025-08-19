afaf248f04d44eaf79ceb5e7858d2f1c5e54326e
Bayana Shaik <bayanashaik@gmail.com>
2025-08-19 15:29:20 +0530

    Added Day 01 solutions and notes

--- day_01/problem_1.py
+++ day_01/problem_1.py
@@ -1,6 +1,11 @@
 # Problem 1: Score of a String
 # LeetCode Link: https://leetcode.com/problems/score-of-a-string/description/
-
+# Description:
+# Given a string s, calculate the "score" of the string.
+# The score is the sum of the absolute differences between ASCII values of adjacent characters.
+# Example: For s = "hello":
+#   ASCII values: h=104, e=101, l=108, l=108, o=111
+#   Score = |104-101| + |101-108| + |108-108| + |108-111| = 13
 class Solution:
     def scoreOfString(self, s: str) -> int:
         score = 0
